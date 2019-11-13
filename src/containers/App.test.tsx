c42e3f2a6e4b8834d85ddf2cc19bd09a34963422
Robin <1105080+openscript@users.noreply.github.com>
2019-11-13 21:02:33 +0100

    Mock store

--- src/containers/App.test.tsx
+++ src/containers/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import createMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { State } from '../models/State';
+import thunk from 'redux-thunk';
+import { ThunkDispatchType } from '../features/FeatureHelpers';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  const mockStore = createMockStore<State, ThunkDispatchType>([thunk]);
+  const store = mockStore({
+    users: [
+      {id: 0, forename: 'Tester', surname: 'Doe', email: 'adama'}
+    ]
+  })
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
